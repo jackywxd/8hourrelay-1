4fafd73258a74a6cc5640bc84b3a573e3117eb7c
Jacky Wu <jacky.wu@heidi.cloud>
2024-06-27 12:56:04 -0700

    Logout current session if could not find user

--- apps/website/src/queries/server/auth.ts
+++ apps/website/src/queries/server/auth.ts
@@ -1,39 +1,47 @@
-import { createClient } from '@/supabase/server'
+import { createClient } from '@/supabase/server';
 
 export async function getAuth() {
-  const supabase = createClient()
+  const supabase = createClient();
   const {
     data: { session },
     error,
-  } = await supabase.auth.getSession()
+  } = await supabase.auth.getSession();
 
   return error || !session
     ? { session: null, user: null }
-    : { session, user: session?.user }
+    : { session, user: session?.user };
 }
 
+export async function logoutUser() {
+  const supabase = createClient();
+  const unsigned = await supabase.auth.signOut();
+
+  if (unsigned?.error) {
+    console.log(`logout error`, unsigned?.error);
+  }
+}
 export async function authenticate() {
-  const supabase = createClient()
+  const supabase = createClient();
   const {
     data: { user },
     error,
-  } = await supabase.auth.getUser()
+  } = await supabase.auth.getUser();
 
   return error || !user
     ? { authenticated: false, user: null }
-    : { authenticated: true, user }
+    : { authenticated: true, user };
 }
 
 export async function authorize(id: string) {
-  const supabase = createClient()
+  const supabase = createClient();
   const {
     data: { user },
     error,
-  } = await supabase.auth.getUser()
+  } = await supabase.auth.getUser();
 
-  if (error || !user) return { authorized: false, user: null }
+  if (error || !user) return { authorized: false, user: null };
 
   return user?.id === id
     ? { authorized: true, user }
-    : { authorized: false, user: null }
+    : { authorized: false, user: null };
 }

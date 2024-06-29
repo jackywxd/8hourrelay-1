1f8ab3e897c82f3ef13894ee82bf96289b06a424
Jacky Wu <jacky.wu@heidi.cloud>
2024-06-29 00:01:38 -0700

    Update styles for my-team

--- apps/website/src/app/(protected)/my-registrations/page.tsx
+++ apps/website/src/app/(protected)/my-registrations/page.tsx
@@ -1,18 +1,18 @@
-import { Suspense } from 'react';
-import { DashboardHeader } from '@/components/header';
-import { RaceEntryCreateButton } from '@/components/race-create-button';
-import RaceEntries from './RaceEntries';
-import { FormSkeleton } from '@/components/FormSkeleton';
 import {
   IUserAllRaceEntries,
   listUserRaceEntries,
 } from '@/actions/raceEntryActions';
+import { FormSkeleton } from '@/components/FormSkeleton';
+import { DashboardHeader } from '@/components/header';
+import { RaceEntryCreateButton } from '@/components/race-create-button';
+import { Suspense } from 'react';
+import RaceEntries from './RaceEntries';
 
 export default async function MyRegistrationsPage() {
   const raceEntries: IUserAllRaceEntries = await listUserRaceEntries();
   console.log(`raceEntries`, raceEntries);
   return (
-    <div>
+    <div className="w-full">
       <DashboardHeader
         heading="My Registration"
         text="Manage your registration. You can register multiple race entries for different persons using different email and phone number."

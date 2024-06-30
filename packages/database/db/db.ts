cc090280fe54bec041b975884dcf3c82c9d529f0
Jacky Wu <jacky.wu@heidi.cloud>
2024-06-30 09:35:50 -0700

    Update database url used by supabase integration

--- packages/database/db/db.ts
+++ packages/database/db/db.ts
@@ -13,7 +13,7 @@ import * as schema from './schema';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL ?? process.env.POSTGRES_URL!;
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 export const client = postgres(connectionString!, { prepare: false });

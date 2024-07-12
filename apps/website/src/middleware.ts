75ab3e03eb037119a4742632ce0b7c5bffef754a
Jacky Wu <jacky.wu@heidi.cloud>
2024-07-11 22:54:18 -0700

    Update middleware

--- apps/website/src/middleware.ts
+++ apps/website/src/middleware.ts
@@ -25,5 +25,5 @@ export async function middleware(request: NextRequest) {
 
 // Improved Configuration with Negative Lookahead
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon\\.ico).*)'],
+  matcher: ['/', '/auth/:path*'],
 };

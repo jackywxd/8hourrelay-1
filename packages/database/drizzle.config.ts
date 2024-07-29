40205f900a02f2ce1da75c03b0a79edf2505fa72
Jacky Wu <jacky.wu@heidi.cloud>
2024-07-29 10:48:19 -0700

    Database schema update for promoCode and new table coupon

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -4,6 +4,7 @@ const stage = process.env.STAGE || 'dev';
 export default {
   dialect: 'postgresql',
   schema: './db/schema.ts',
+  out: './drizzle',
   dbCredentials: {
     url: process.env.DATABASE_URL ?? process.env.POSTGRES_URL!,
   },

b3fd998f7c3f24730a1f923050495faf73c220fd
Jacky Wu <jacky.wu@heidi.cloud>
2024-08-15 11:31:07 -0700

    Bugfix: 1. Nested <Link> component prevents payment link access 2. Team API filtered unpaid race entries

--- apps/website/src/components/RaceEntriesList.tsx
+++ apps/website/src/components/RaceEntriesList.tsx
@@ -30,10 +30,10 @@ const RaceEntryItem = ({
   if (!raceEntry) return null;
   const race = raceEntry;
   return (
-    <Link href={`/my-registrations/entry/${raceEntry.id}`}>
-      <div className="group relative mt-3 flex rounded-lg bg-muted px-2 py-4 shadow-md transition hover:bg-accent hover:text-accent-foreground focus:outline-none aria-checked:border-indigo-500 aria-checked:ring-1 aria-checked:ring-indigo-500 aria-checked:ring-offset-1">
+    <div className="group relative mt-3 flex w-full rounded-lg bg-muted px-2 py-4 shadow-md transition hover:bg-accent hover:text-accent-foreground focus:outline-none aria-checked:border-indigo-500 aria-checked:ring-1 aria-checked:ring-indigo-500 aria-checked:ring-offset-1">
+      <div className="flex w-full items-center justify-around">
         <div className="hidden w-1/4 items-center justify-between gap-4 md:block">
-          <Avatar className="h-12 w-12 mix-blend-difference">
+          <Avatar className="mx-auto h-12 w-12 mix-blend-difference">
             <AvatarImage
               src={
                 raceEntry.race?.name === 'Master' ||
@@ -52,40 +52,45 @@ const RaceEntryItem = ({
             </AvatarFallback>
           </Avatar>
         </div>
-        <div className="flex w-full items-center justify-around">
-          <>
-            <span>
+        <Link
+          className="w-full"
+          href={`/my-registrations/entry/${raceEntry.id}`}
+        >
+          <div className="flex w-full items-center justify-around">
+            <div>
               {capitalize(
                 race?.preferName
                   ? race.preferName
                   : `${race?.firstName} ${race?.lastName}`
               )}
-            </span>
-            <span>{race.email}</span>
-          </>
+            </div>
+            <div>{race.email}</div>
+          </div>
+        </Link>
+        <div className="w-1/4 items-center justify-between">
           <Link href={`/teams/${encodeURIComponent(raceEntry.team.name)}`}>
             <span className="hidden items-center text-sm font-medium uppercase leading-none underline md:block">
               {raceEntry.team.name}
             </span>
           </Link>
-          <div className="hidden md:block">
-            {raceEntry.session?.paymentStatus === 'paid' ? (
-              <Icons.check className="h-6 w-6 text-green-500" />
-            ) : (
-              <div>
-                <div>
-                  <Link
-                    href={`/payment?session=${raceEntry.session?.sessionId}`}
-                    target="_blank"
-                  >
-                    Payment
-                  </Link>
-                </div>
-              </div>
-            )}
-          </div>
         </div>
       </div>
-    </Link>
+      <div className="flex w-1/4 items-center justify-center">
+        {raceEntry.session?.paymentStatus === 'paid' ? (
+          <Icons.check className="h-6 w-6 text-green-500" />
+        ) : (
+          <div>
+            <div>
+              <Link
+                href={`/payment?session=${raceEntry.session?.sessionId}`}
+                target="_blank"
+              >
+                <span>Pay</span>
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
